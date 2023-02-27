57b53234ee577b0dc676deddc3dfbd9488d8d263
Mohammad Muzafer <mohammad.muzafer@codecraft.co.in>
2023-02-27 16:03:35 +0530

    created react app with storybook

--- src/components/Counter/types.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface IProps {
-  /**
-   *  Can pass an initial value to be used for the Counter, by default it will be zero
-   *
-   */
-  initialCount?: number;
-}
